c7eaa3749b182051df4048fdc40ae2645e7d415b
Harikoharihari2525 <michisata914@gmail.com>
2025-09-13 11:29:13 +0900

    Update KatoIchiro_Chapter18.java

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -1,5 +1,18 @@
 package kadai_018;
 
-public class KatoIchiro_Chapter18 {
+public class KatoIchiro_Chapter18 extends Kato_Chapter18{
+	
+	public KatoIchiro_Chapter18(String familyName, String address) {
+        super(familyName, "", address);
+        setGivenName();
+    }
 
+    public void setGivenName() {
+        this.givenName = "一郎";
+    }
+
+    @Override
+    public void eachIntroduce() {
+        System.out.println("好きな食べ物はリンゴです");
+    }
 }
