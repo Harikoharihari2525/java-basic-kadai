78f16536965bd86aaa77c1f18ed01db554c71500
Harikoharihari2525 <michisata914@gmail.com>
2025-09-13 11:13:37 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,6 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+	public void setGivenName()
+
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+
+public class Kato_Chapter18{
+
+   String familyName;
+   String givenName;
+   String address;
+
+}
+
+abstract public class Kato_Chapter18 {
+	
+
+	
+	
+	abstract void commonlntroduce();
+	abstract void eachIntroduce	();
+	abstract void execIntroduce();
+
+}
