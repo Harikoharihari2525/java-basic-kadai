69a86bb4e7530884abebded555b9e6b0e876316d
Harikoharihari2525 <michisata914@gmail.com>
2025-09-15 22:51:56 +0900

    Update KatoTaro_Chapter18.java

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -1,7 +1,8 @@
 package kadai_018;
 
 public class KatoTaro_Chapter18 extends Kato_Chapter18 {
-    public void setGivenName() {
+	public KatoTaro_Chapter18(String familyName, String address) {
+        super(familyName, address);
         this.givenName = "太郎";
     }
 
