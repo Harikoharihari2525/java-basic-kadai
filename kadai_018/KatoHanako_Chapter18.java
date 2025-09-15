d259e91dbbbbe57ed0194e7913620f47c12c1314
Harikoharihari2525 <michisata914@gmail.com>
2025-09-15 19:59:54 +0900

    Update KatoHanako_Chapter18.java

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -1,7 +1,8 @@
 package kadai_018;
 
 public class KatoHanako_Chapter18 extends Kato_Chapter18{
-	public void setGivenName() {
+	public KatoHanako_Chapter18(String familyName, String address) {
+        super(familyName, address); 
         this.givenName = "花子";
     }
 
