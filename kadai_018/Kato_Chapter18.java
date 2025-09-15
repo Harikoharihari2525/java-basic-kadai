674c61ded70717d5da25ffdac9a85b283bf9a625
Harikoharihari2525 <michisata914@gmail.com>
2025-09-15 19:26:42 +0900

    Update Kato_Chapter18.java

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -1,16 +1,9 @@
 package kadai_018;
 
 public abstract class Kato_Chapter18 {
-
-    public String familyName;
+	public String familyName;
     public String givenName;
     public String address;
-
-    public Kato_Chapter18(String familyName, String givenName, String address) {
-        this.familyName = familyName;
-        this.givenName = givenName;
-        this.address = address;
-    }
 
     public void commonIntroduce() {
         System.out.println("名前は" + familyName + givenName + "です");
@@ -22,6 +15,6 @@ public abstract class Kato_Chapter18 {
     public void execIntroduce() {
         commonIntroduce();
         eachIntroduce();
-        System.out.println();
     }
 }
+
